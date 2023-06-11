d95532e58e516074aedeb8169468375fdf07919c
yash <yash.k@beehyv.com>
2023-06-11 21:28:40 +0530

    first commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+it("renders welcome message", () => {
+  render(<App />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+});
